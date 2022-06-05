a62fd5784fb76fe88435fe0310639a915009feee
chasemoses <43392487+chasemoses@users.noreply.github.com>
2022-06-04 22:28:39 -0600

    Auth0 Designs routes
    Added Auth0 authentication requirements to all routes for designs. Only managers/owner can view this.

--- routes/design.js
+++ routes/design.js
@@ -1,25 +1,26 @@
 const router = require('express').Router();
+const {requiresAuth} = require('express-openid-connect')
 
 
 // Require Controller
 const design = require('../controllers/designs');
 
 // GET Designs
-router.get('/designs', design.getDesigns);
+router.get('/designs', requiresAuth(), design.getDesigns);
 
 
 // // GET Design
-router.get('/designs/:id', design.getDesignById, design.getDesign);
+router.get('/designs/:id', requiresAuth(), design.getDesignById, design.getDesign);
 
 
 // POST Design (This should be restricted as we don't want customers creating Designs.)
-router.post('/designs', design.createDesignItem);
+router.post('/designs', requiresAuth(), design.createDesignItem);
 
 // Delete Design
-router.delete('/designs/:id', design.getDesignById, design.deleteDesign);
+router.delete('/designs/:id', requiresAuth(), design.getDesignById, design.deleteDesign);
 
 // Update Design (Probably restrict this to authorized users.)
-router.put('/designs/:id', design.getDesignById, design.updateDesign);
+router.put('/designs/:id', requiresAuth(), design.getDesignById, design.updateDesign);
 
 
 
