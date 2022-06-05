021a6706e4ebfde9c04c03eb166b75d8de442e1d
chasemoses <43392487+chasemoses@users.noreply.github.com>
2022-06-04 22:06:04 -0600

    Moved Auth0 config to new folder
    Moved Auth0 config to new folder.

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,25 +1,26 @@
 const router = require('express').Router();
+const {requiresAuth} = require('express-openid-connect')
 
 
 // Require Controller
 const inventory = require('../controllers/inventory');
 
 // GET Inventory items
-router.get('/inventory', inventory.getInventoryItems);
+router.get('/inventory', requiresAuth(), inventory.getInventoryItems);
 
 
 // GET Inventory Item
-router.get('/inventory/:id', inventory.getInventoryById, inventory.getInventoryItem);
+router.get('/inventory/:id', requiresAuth(), inventory.getInventoryById, inventory.getInventoryItem);
 
 
 // POST Inventory Item (This should be restricted as we don't want customers creating products.)
-router.post('/inventory', inventory.createInventoryItem);
+router.post('/inventory', requiresAuth(),  inventory.createInventoryItem);
 
 // Delete Inventory Item
-router.delete('/inventory/:id', inventory.getInventoryById, inventory.deleteInventoryItem);
+router.delete('/inventory/:id', requiresAuth(), inventory.getInventoryById, inventory.deleteInventoryItem);
 
 // Update Inventory Item (Probably restrict this to authorized users.)
-router.put('/inventory/:id', inventory.getInventoryById, inventory.updateInventoryItem);
+router.put('/inventory/:id', requiresAuth(), inventory.getInventoryById, inventory.updateInventoryItem);
 
 
 
--- server.js
+++ server.js
@@ -2,22 +2,14 @@ const express = require('express');
 const mongodb = require('./database/mongodb');
 const app = express();
 const port = process.env.PORT || 8080;
+const auth0config = require('./auth0/config');
 
-const { auth } = require('express-openid-connect');
-
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.SECRET,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL
-};
+const { auth, requiresAuth } = require('express-openid-connect');
 
 
 app
 // auth router attaches /login, /logout, and /callback routes to the baseURL
-.use(auth(config))
+.use(auth(auth0config))
 // req.isAuthenticated is provided from the auth router
 .get('/', (req, res) => {
     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
@@ -34,3 +26,5 @@ mongodb.connectDB();
 
 app.listen(port, () =>
 console.log(`Running on port ${port}`));
+
+module.exports = requiresAuth;
